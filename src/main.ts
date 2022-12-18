2a70897a77da69e639f5e0bd8e01a4a738d51c84
PleBea <bini060315kim@gmail.com>
2022-12-19 01:51:36 +0900

    chore: Setup swagger and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+  if (config.get('NODE_ENV') !== 'production') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('CATCTF API')
+      .setDescription('For CATCTF')
+      .setVersion('1.0')
+      .addTag('REST')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('document', app, document);
+  }
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: config.get<string>('CORS_ORIGIN', '*'),
+    methods: config.get<string>('CORS_METHODS', 'GET,PUT,POST,DELETE'),
+    credentials: config.get<boolean>('CORS_CREDENTIALS', true),
+    preflightContinue: config.get<boolean>('CORS_PREFLIGHT', false),
+    optionsSuccessStatus: config.get<number>('CORS_OPTIONS_STATUS', 204),
+  });
   await app.listen(3000);
 }
 bootstrap();
